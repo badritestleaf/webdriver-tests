719519ab47c231e266492acab8a55f82e94127e5
Testleaf <babu@testleaf.com>
2023-09-02 12:12:27 +0530

    Testcase 103 written for story 112

---
+++ src/main/java/com/leaftaps/testcases/TC103_EditLead.java
@@ -0,0 +1,28 @@
+package com.leaftaps.testcases;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.framework.testng.api.base.ProjectHooks;
+import com.leaftaps.pages.LoginPage;
+
+public class TC103_EditLead extends ProjectHooks{
+	@BeforeTest
+	public void setValues() {
+		testcaseName = "Create Lead";
+		testDescription ="Create Lead to Leaftaps";
+		authors="Babu";
+		category ="DevQAOps Practise";
+		excelFileName="Login";
+	}
+	
+	@Test(dataProvider = "fetchData")
+	public void runLogin(String username, String password) {
+		new LoginPage()
+		.enterUsername(username)
+		.enterPassword(password)
+		.clickLogin();
+
+	}
+
+}
